69cb5cd6bf20541d9555503e7dfb6477d982f225
Michael Dang <dominhation@hotmail.com>
2024-02-06 23:41:02 -0800

    adding tester stage

--- jenkins-docker/test.sh
+++ jenkins-docker/test.sh
@@ -1,6 +1,6 @@
 docker run -d --name ubuntu-tester --network wordle-network ubuntu-image
 curl -X 'POST' \
-  'http://localhost:9400/game/new?username=ucohen' \
+  'orc:9400/game/new?username=ucohen' \
   -H 'accept: application/json' \
   -d '' > first_curl.txt
 
