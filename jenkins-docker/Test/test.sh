bf40e9132c819c0db82b679c1cef63bc81425dbf
Michael Dang <dominhation@hotmail.com>
2024-02-07 01:21:40 -0800

    testing new Tester container

--- jenkins-docker/Test/test.sh
+++ jenkins-docker/Test/test.sh
@@ -1,3 +1,6 @@
 echo 'Buidling wordle-api-tester...'
 docker build    -t wordle-api-tester-image ./jenkins-docker/Test
 docker run -d --name wordle-api-tester --network wordle-network wordle-api-tester-image
+
+echo 'Check if container is on the network...'
+docker network inspect --format='{{range $container_id,$conf := .Containers}} {{println $conf.Name $container_id}} {{end}}' wordle-network
