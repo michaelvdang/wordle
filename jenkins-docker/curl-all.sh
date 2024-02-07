7d4c8ffaccf40c12fd7b963c7d7cf562cd8cad30
Michael Dang <dominhation@hotmail.com>
2024-02-06 22:37:10 -0800

    refactoring Jenkins build section

--- jenkins-docker/curl-all.sh
+++ jenkins-docker/curl-all.sh
@@ -1,10 +1,10 @@
 # curl orc:9400 >> output.txt
 curl stats:9000 > output.txt 
-echo -e '\n'
+echo '' >> output.txt
 curl wordcheck:9100 >> output.txt 
-echo -e '\n'
+echo '' >> output.txt
 curl wordvalidation:9200 >> output.txt 
-echo -e '\n'
+echo '' >> output.txt
 curl play:9300 >> output.txt 
-echo -e '\n'
+echo '' >> output.txt
 curl orc:9400 >> output.txt
