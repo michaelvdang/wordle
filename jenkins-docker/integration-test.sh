7d76c35c47309c2174b90b7e1ead7bbcf1e3b666
Michael Dang <dominhation@hotmail.com>
2024-02-06 16:28:39 -0800

    new batch files for local testing

--- jenkins-docker/integration-test.sh
+++ jenkins-docker/integration-test.sh
@@ -1,10 +1,2 @@
 pwd
-curl stats:9000 > app.txt
-curl wordcheck:9100 > app.txt
-curl wordvalidation:9200 > app.txt
-curl google.com > google.txt
-curl play:9300 > app.txt
-curl orc:9400  > app.txt
-
-cat app.txt
-cat google.txt
+curl stats:9000 && curl wordcheck:9100 && curl wordvalidation:9200 && curl play:9300 && curl orc:9400
