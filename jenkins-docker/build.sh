df74bdfff21d6f4f31969b0ff4d06c65e3a0386d
Michael Dang <dominhation@hotmail.com>
2024-02-07 02:27:47 -0800

    adding Redis to satisfy orc dependency

--- jenkins-docker/build.sh
+++ jenkins-docker/build.sh
@@ -3,21 +3,25 @@ docker ps
 docker ps -a
 echo 'Creaeting new wordle-network...'
 docker network create wordle-network
+
+# echo 'Build and run Redis container..'
+# docker run -d --name redis -p 0.0.0.0:6379:6379 --network wordle-network redis:alpine redis-server redis.conf
+
 echo 'Build and run Stats container..'
 docker build    -t stats-image ./app/services/Stats
 docker run -d --name stats -p 9000:9000 --network wordle-network stats-image
 
 echo 'Build and run WordCheck container...'
 docker build    -t wordcheck-image ./app/services/WordCheck
-docker run -d --name wordcheck -p 9100:9100 -h localhost --network wordle-network  wordcheck-image
+docker run -d --name wordcheck -p 0.0.0.0:9100:9100 -h localhost --network wordle-network  wordcheck-image
 
 echo 'Build and run WordValidation container...'
 docker build    -t wordvalidation-image ./app/services/WordValidation
-docker run -d --name wordvalidation -p 9200:9200 -h localhost --network wordle-network wordvalidation-image
+docker run -d --name wordvalidation -p 0.0.0.0:9200:9200 -h localhost --network wordle-network wordvalidation-image
 
 echo 'Build and run play container...'
 docker build    -t play-image ./app/services/Play
-docker run -d --name play -p 9300:9300 -h localhost --network wordle-network play-image
+docker run -d --name play -p 0.0.0.0:9300:9300 -h localhost --network wordle-network play-image
 
 echo 'Build and run orc container...'
 docker build    -t orc-image .
