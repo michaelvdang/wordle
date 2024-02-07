c7148bcb0ae89f7a31971545b70ff8f4ffba7b9a
Michael Dang <dominhation@hotmail.com>
2024-02-06 20:25:26 -0800

    all containers are up

--- jenkins-docker/curl-all.sh
+++ jenkins-docker/curl-all.sh
@@ -1,2 +1,6 @@
 # curl orc:9400 >> output.txt
-curl stats:9000 > output.txt && curl wordcheck:9100 >> output.txt && curl wordvalidation:9200 >> output.txt && curl play:9300 >> output.txt && curl orc:9400 >> output.txt
+curl stats:9000 > output.txt 
+curl wordcheck:9100 >> output.txt 
+curl wordvalidation:9200 >> output.txt 
+curl play:9300 >> output.txt 
+curl orc:9400 >> output.txt
