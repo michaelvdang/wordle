a4e97a79315e9b37231f4bf293d308e8b277c7c7
Michael Dang <dominhation@hotmail.com>
2024-02-07 22:39:12 -0800

    getting curl orc to work

--- jenkins-docker/Test/test.sh
+++ jenkins-docker/Test/test.sh
@@ -2,7 +2,7 @@
 echo ===================Testing the api===================
 echo 'Buidling wordle-api-tester...'
 docker build    -t wordle-api-tester-image ./jenkins-docker/Test
-docker run -d --name wordle-api-tester --network wordle-network wordle-api-tester-image
+docker run -d --volume ./:/ --name wordle-api-tester --network wordle-network wordle-api-tester-image
 
 echo 'Check if container is on the network...'
 docker network inspect --format='{{range $container_id,$conf := .Containers}} {{println $conf.Name $container_id}} {{end}}' wordle-network
