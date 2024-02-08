b2e50266340e10ae8a14d02c7d5520080778b152
Michael Dang <dominhation@hotmail.com>
2024-02-07 16:52:26 -0800

    adding Redis container to satisfy orc dependency

--- jenkins-docker/build.sh
+++ jenkins-docker/build.sh
@@ -6,7 +6,7 @@ echo 'Creating new wordle-network...'
 docker network create wordle-network
 
 echo 'Build and run Redis container..'
-docker run -d --name redis -p 0.0.0.0:6379:6379 --network wordle-network -v "$(pwd)"/redis.conf:/data/redis.conf redis:alpine redis-server /data/redis.conf
+docker run -d --name redis -p 0.0.0.0:6379:6379 --network wordle-network -v ./redis.conf:/data/redis.conf redis:alpine redis-server /data/redis.conf
 # docker run -d --name redis -p 0.0.0.0:6379:6379 --network wordle-network -v ./redis.conf:/data/redis.conf redis:alpine redis-server /data/redis.conf
 
 echo 'Build and run Stats container..'
