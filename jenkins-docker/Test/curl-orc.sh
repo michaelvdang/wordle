51cb7d703d506ae67a2bd015bf661b54dda5225a
Michael Dang <dominhation@hotmail.com>
2024-02-07 23:25:59 -0800

    getting curl orc to work

--- jenkins-docker/Test/curl-orc.sh
+++ jenkins-docker/Test/curl-orc.sh
@@ -1,5 +1,5 @@
-curl --no-progress-meter 'wordvalidation:9200/' > /output/wv.txt
-curl --no-progress-meter 'wordvalidation:9200/word/is-valid/house' >> /output/wv.txt
+curl --no-progress-meter 'wordvalidation:9200/' > wv.txt
+curl --no-progress-meter 'wordvalidation:9200/word/is-valid/house' >> wv.txt
 # curl 'orc:9400/game/restore?username=ucohen&game_id=100' > /output/orc.txt
 # curl 'play:9300/play?guid=al%3Bskdjf&game_id=100' > /output/play.txt
 # curl 'wordcheck:9100/answers/count' > /output/wc.txt
