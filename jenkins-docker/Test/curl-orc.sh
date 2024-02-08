ae87a8fd3269311905236915671e576a204c75e5
Michael Dang <dominhation@hotmail.com>
2024-02-07 22:28:22 -0800

    getting curl orc to work

--- jenkins-docker/Test/curl-orc.sh
+++ jenkins-docker/Test/curl-orc.sh
@@ -1,2 +1,3 @@
-curl -X 'POST' 'localhost:9400/game/new?username=ucohen'
+curl 'wordvalidation:9200/word/is-valid/house'
+curl -X 'POST' 'orc:9400/game/new?username=ucohen'
 # curl -X 'POST' 'orc:9400/game/new?username=ucohen' -H 'accept: application/json' -d '' > first_curl.txt
