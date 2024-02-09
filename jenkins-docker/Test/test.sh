2060d9d07df4ea126538f2460847dff01dbd065f
Michael Dang <dominhation@hotmail.com>
2024-02-08 22:59:13 -0800

    export log files

--- jenkins-docker/Test/test.sh
+++ jenkins-docker/Test/test.sh
@@ -2,7 +2,7 @@
 echo ===================Testing the api===================
 echo 'Buidling wordle-api-tester...'
 docker build    -t wordle-api-tester-image ./jenkins-docker/Test
-docker run -d --volume wordle-db:/data --volume ./:/data --name wordle-api-tester --network wordle-network wordle-api-tester-image
+docker run -d --volume ./:/data --name wordle-api-tester --network wordle-network wordle-api-tester-image
 
 echo 'Check if container is on the network...'
 docker network inspect --format='{{range $container_id,$conf := .Containers}} {{println $conf.Name $container_id}} {{end}}' wordle-network
@@ -13,12 +13,12 @@ docker exec redis redis-cli -a $REDIS_SECRET ping
 
 
 sh 'sleep 5'
-echo 'After test logs from wordvalidation:'
-sh 'docker logs wordvalidation'
-echo 'After test logs from play:'
-sh 'docker logs play'
-echo 'After test logs from orc:'
-sh 'docker logs orc'
+echo 'After-test logs from wordvalidation:'
+sh 'docker logs wordvalidation > wv-log.txt'
+echo 'After-test logs from play:'
+sh 'docker logs play > play-log.txt'
+echo 'After-test logs from orc:'
+sh 'docker logs orc > orc-log.txt'
 sh '''
   echo ''
   echo 'Log from wordle-api-tester'
