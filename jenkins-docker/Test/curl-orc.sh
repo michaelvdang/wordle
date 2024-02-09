cd90c89097555e5182b412ac16ecd33ca470bd26
Michael Dang <dominhation@hotmail.com>
2024-02-08 21:04:19 -0800

    backend integration testing

--- jenkins-docker/Test/curl-orc.sh
+++ jenkins-docker/Test/curl-orc.sh
@@ -1,6 +1,7 @@
 ## called when the wordle-api-tester container is run
 curl --no-progress-meter -X 'POST' 'orc:9400/game/new?username=ucohen' >> output.txt
-cat output.txt | jq '.guid'
+GUID=`cat output.txt | jq '.guid'`
+echo $GUID
 # curl --no-progress-meter 'wordvalidation:9200/' >> output.txt
 # curl --no-progress-meter 'wordvalidation:9200/word/is-valid/house' >> output.txt
 # curl --no-progress-meter 'orc:9400/game/restore?username=ucohen&game_id=100' >> output.txt
