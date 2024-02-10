93d138dadb45ce7c0faa8bc9f1bbb7864b5ad473
Michael Dang <dominhation@hotmail.com>
2024-02-09 19:35:00 -0800

    Clean up Jenkinsfile

---
+++ jenkins-docker/Pre-Build/pre-build.sh
@@ -0,0 +1,5 @@
+chmod u+x -R ./jenkins-docker
+cat $ENV_FILE_PATH > .env
+cat $ENV_FILE_PATH > app/services/Stats/.env
+cat $ENV_FILE_PATH > app/services/Play/.env
+cat $REDIS_CONF_FILE_PATH > app/services/Redis/redis.conf
