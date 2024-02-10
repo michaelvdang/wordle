489d6969389eff9b7ddb753179e111f025be047b
Michael Dang <dominhation@hotmail.com>
2024-02-09 20:06:48 -0800

    Run TopTen.py from orc build

--- bin/TopTen.py
+++ bin/TopTen.py
@@ -25,6 +25,7 @@ r = redis.Redis(host=host, port=6379, decode_responses=True, password=REDISCLI_A
 # for when running services in docker in ubuntu with shared volume and
 #     and setting crontab manually in terminal
 path = ''
+print('CAN WE GET PATH NAME FROM HERE?')
 if (os.name == 'posix'):
   if (os.path.isfile('/var/snap/docker/common/var-lib-docker/volumes/wordle_db/_data/game1.db')): # Docker on Windows doesn't have /var/snap
   #('Running in Docker-Ubuntu that was installed by snap')
@@ -32,7 +33,7 @@ if (os.name == 'posix'):
   # Running in Docker-Ubuntu that was installed not by snap
   elif (os.path.isfile('/var/lib/docker/volumes/wordle_db/_data/game1.db')):
     path = '/var/lib/docker/volumes/wordle_db/_data/'
-  elif (os.path.isfile('../var/game1.db')):
+  elif (os.path.isfile('./var/game1.db')):
     path = '../var/game1.db'
   else:
     raise FileNotFoundError('Could not find game1.db in /var/snap/docker/common/var-lib-docker/volumes/wordle_db/_data/ or /var/lib/docker/volumes/wordle_db/_data/')
