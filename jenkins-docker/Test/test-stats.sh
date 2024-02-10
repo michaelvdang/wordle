ecb273b5227f5799b104557ae39fe6fde33833cb
Michael Dang <dominhation@hotmail.com>
2024-02-09 20:00:28 -0800

    Querying top streaks and winners

--- jenkins-docker/Test/test-stats.sh
+++ jenkins-docker/Test/test-stats.sh
@@ -1,9 +1,12 @@
-echo UTC date and time: `date +%m-%d\ %T` > /data/output.txt
+echo UTC date and time: `date +%m-%d\ %T` > /data/stats-output.txt
 ## Testing 3 endpoints in the stats API: Get User Stats, Get Username, Get User Id
 ## scenario 1:
 # 1. Create random user
 # 2. 
 # 2. Create new game
 # 3. Add 6 guesses to complete game
+
+echo Querying top streaks and winners: stats:9000/stats/top-streaks-and-winners >> /data/stats-output.txt
+curl stats:9000/stats/top-streaks-and-winners >> /data/stats-output.txt
 
 USERNAME=`bash random-string.sh`
