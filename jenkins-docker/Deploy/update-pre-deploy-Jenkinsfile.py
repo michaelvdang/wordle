5d4c1e23a42722b562d0ed70b0d5c7a10c22af30
Michael Dang <dominhation@hotmail.com>
2024-02-10 17:02:10 -0800

    adding pre-deploy step

---
+++ jenkins-docker/Deploy/update-pre-deploy-Jenkinsfile.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SERVER_IP = os.environ.get('SERVER_IP')
+
+placeholder = '<IP_ADDRESS>'
+
+with open(r'/wordle/jenkins-docker/Deploy/Jenkinsfile', 'r') as file:
+  data = file.read()
+
+  data = data.replace(placeholder, SERVER_IP)
+
+with open(r'/wordle/jenkins-docker/Deploy/Jenkinsfile', 'r') as file:
+  file.write(data)
+
+print('Text replaced')
