1bab9862c64b8d9f482ca5dc51f6a39301398dd8
Michael Dang <dominhation@hotmail.com>
2024-02-11 22:18:25 -0800

    extract IP_ADDRESS to file

--- jenkins-docker/Deploy/extract-ip-address.py
+++ jenkins-docker/Deploy/extract-ip-address.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 SERVER_IP = os.environ.get('SERVER_IP')
 
-with open(r'/wordle/jenkins-docker/Deploy/Jenkinsfile', 'w') as file:
+with open(r'/wordle/jenkins-docker/Deploy/IP_ADDRESS.env', 'w') as file:
   file.write(SERVER_IP)
 
 print('SERVER_IP extracted')
