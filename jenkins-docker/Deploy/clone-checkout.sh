fbd199f1ebc2a73a9eb80d6831ffd78306d0794e
Michael Dang <dominhation@hotmail.com>
2024-02-13 14:00:44 -0800

    refactor app folder setup

--- jenkins-docker/Deploy/clone-checkout.sh
+++ jenkins-docker/Deploy/clone-checkout.sh
@@ -6,12 +6,15 @@ date >> timestamp.txt
 # check if repo has been cloned already, if yes stop containers and erase, then clone new repo
 if [ -d wordle ]
 then 
+  ls -al
   cd wordle
   docker compose down
   cd 
   sudo rm -rf wordle
+  ls -al
 fi
 
 git clone https://github.com/michaelvdang/wordle.git
 cd wordle
 git checkout docker-jenkins
+cat Orchestrator.py
--- jenkins-docker/Pre-Build/setup-env.sh
+++ jenkins-docker/Pre-Build/setup-env.sh
@@ -1,4 +1,4 @@
 cat $ENV_FILE_PATH > .env
-cat $ENV_FILE_PATH > app/services/Stats/.env
-cat $ENV_FILE_PATH > app/services/Play/.env
+# cat $ENV_FILE_PATH > app/services/Stats/.env
+# cat $ENV_FILE_PATH > app/services/Play/.env
 cat $REDIS_CONF_FILE_PATH > app/services/Redis/redis.conf
