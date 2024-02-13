c5a62a7cad71437c267911c87dcc61c00aad70c9
Michael Dang <dominhation@hotmail.com>
2024-02-12 18:36:50 -0800

    Add further instructions for setting up server

--- bin/server-init.sh
+++ bin/server-init.sh
@@ -69,7 +69,7 @@ source .env
 
   # remove carriage return from $SERVER_IP
 SERVER_IP=`echo $SERVER_IP | tr -d "\r" | cat -v`
-DOMAIN_NAME=`echo $DOMAIN_NAME | tr -d "\r" | cat -v`
+# DOMAIN_NAME=`echo $DOMAIN_NAME | tr -d "\r" | cat -v`
 VITE_DOMAIN_NAME=`echo $VITE_DOMAIN_NAME | tr -d "\r" | cat -v`
 VITE_BACK_END_TYPE=`echo $VITE_BACK_END_TYPE | tr -d "\r" | cat -v`
 
@@ -78,21 +78,21 @@ echo VITE_DOMAIN_NAME=$VITE_DOMAIN_NAME > /home/${USER}/wordle/wordle-frontend/.
 echo VITE_BACK_END_TYPE=$VITE_BACK_END_TYPE >> /home/${USER}/wordle/wordle-frontend/.env
 
   # copy template to new file
-sudo cat nginx-template.conf > $DOMAIN_NAME.conf
+sudo cat nginx-template.conf > $VITE_DOMAIN_NAME.conf
   # replace <SERVER_IP> in config file with SERVER_IP from .env
-sed -i "s/<SERVER_IP>/$SERVER_IP/g" $DOMAIN_NAME.conf
-sed -i "s/<DOMAIN_NAME>/$DOMAIN_NAME/g" $DOMAIN_NAME.conf
+sed -i "s/<SERVER_IP>/$SERVER_IP/g" $VITE_DOMAIN_NAME.conf
+sed -i "s/<DOMAIN_NAME>/$VITE_DOMAIN_NAME/g" $VITE_DOMAIN_NAME.conf
   # move config file and create soft link
-sudo mv $DOMAIN_NAME.conf /etc/nginx/sites-available
+sudo mv $VITE_DOMAIN_NAME.conf /etc/nginx/sites-available
   # remove old links
-sudo rm /etc/nginx/sites-enabled/$DOMAIN_NAME
-sudo ln -s /etc/nginx/sites-available/$DOMAIN_NAME.conf /etc/nginx/sites-enabled/$DOMAIN_NAME
+sudo rm /etc/nginx/sites-enabled/$VITE_DOMAIN_NAME
+sudo ln -s /etc/nginx/sites-available/$VITE_DOMAIN_NAME.conf /etc/nginx/sites-enabled/$VITE_DOMAIN_NAME
 
 sudo nginx -s reload
 
 ## make NGINX static files directory
-sudo mkdir -p /var/www/$DOMAIN_NAME
-sudo mkdir -p /var/www/$DOMAIN_NAME/wordle
+sudo mkdir -p /var/www/$VITE_DOMAIN_NAME
+sudo mkdir -p /var/www/$VITE_DOMAIN_NAME/wordle
 
 ## METHOD 1: build the app ON server
 cd /home/$USER/wordle/wordle-frontend
@@ -118,9 +118,10 @@ npm run build
 # read -p "Before movign on, if we are not building the app on server, make sure to run scp-fe-build.bat with the correct SERVER_IP to copy html assets to server. Press ENTER to continue..."
 # sudo mkdir -p /home/$USER/wordle/wordle-frontend/dist
 
-sudo cp -r /home/$USER/wordle/wordle-frontend/dist/* /var/www/$DOMAIN_NAME/wordle/
+sudo cp -r /home/$USER/wordle/wordle-frontend/dist/* /var/www/$VITE_DOMAIN_NAME/wordle/
 
 echo App can now be accessed at $SERVER_IP/wordle
 echo Next step: acquire a domain name and point DNS to $SERVER_IP, then get a SSL certificate with Let's Encrypt's certbot
 echo Finally: update the .env file with new domain name and rerun Jenkins with the updated .env
-sleep 10
+
+echo How to run certbot according to instructions from https://certbot.eff.org/
